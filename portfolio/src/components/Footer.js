7ae3387eab456831423bb74b37e8d35ff292b3ca
Ariel Khawly <akhawly59@gmail.com>
2020-04-10 19:41:06 -0700

    component creation

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <p>Made by: Ariel Khawly</p>
+            <p>Contact information: <a href="mailto:akhawly1@gmail.com">
+                akhawly1@gmail.com</a>.</p>
+        </footer>
+    )
+}
---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,27 @@
+import React, {Fragment} from "react";
+
+function Projects() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-3">
+                    <a target="_blank" href=" https://arielkhawly.github.io/project-1/"> Last Minute Date Generator </a>
+                    <img id="xfiles" src="date.png">
+                </div>
+                <div className="col-sm-3">Project 2
+                    <img id="twinpeaks" src="twinpeaks.jpg">
+                </div>
+                <div className="col-sm-3">Project 3
+                    <img id="joan" src="joan.jpg">
+                </div>
+                <div className="col-sm-3">Project 4
+                    <img id="deb" src="blondie.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+    )
+}
+
+export default Projects
