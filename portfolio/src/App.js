0dfd49c21fd8b5531f2012cc4eb5540b4563af4c
Ariel Khawly <akhawly59@gmail.com>
2020-04-12 14:10:57 -0700

    fixed router and made sure all functional components are imported

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,33 @@
-import React, { Fragment } from 'react';
-import './App.css';
+import React from 'react';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Projects from "./pages/Projects"
+import About from './pages/About'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <Fragment>
-      <Header />
-      <Projects />
-      <Footer />
-    </Fragment>
+    <Router>
+      <Switch>
+        <Route absolute path="/">
+          <Header />
+          <Home />
+          <Footer />
+        </Route>
+        <Route absolute path="/About">
+          <Header />
+          <About />
+          <Footer />
+         </Route>
+        <Route absolute path="/Projects">
+          <Header />
+          <Projects />
+          <Footer />
+         </Route>
+      </Switch>
+  </Router>
   );
 }
 
