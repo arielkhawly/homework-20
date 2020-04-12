d1bf8a34b3bb19e0e508ecbf70758f98d5922bcd
Ariel Khawly <akhawly59@gmail.com>
2020-04-12 14:00:43 -0700

    working on project links and changed class to className

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -2,24 +2,21 @@ import React from "react";
 
 function Header() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand">Ariel Khawly</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" >Home <span class="sr-only">(current)</span></a>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" >Home <span className="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" >Features</a>
+      <li className="nav-item">
+        <a className="nav-link" >About Me</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" >Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled">Disabled</a>
+      <li className="nav-item">
+        <a className="nav-link" >Projects</a>
       </li>
     </ul>
   </div>
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -3,7 +3,11 @@ import React, { Fragment } from "react";
 function Projects() {
     return (
         <div>
-            <iframe title='project 1' src="https://arielkhawly.github.io/project-1/"> Last Minute Date Generator </iframe>
+            <iframe title='project 1' target='_blank' src="https://arielkhawly.github.io/project-1/"> Last Minute Date Generator </iframe>
+            <a src='https://arielkhawly.github.io/project-1/' target='_blank' rel="noopener noreferrer"/>
+            <iframe title='project 2' taget='_blank' src=" https://gentle-bayou-98216.herokuapp.com/"> The Boxing Office </iframe>
+            <iframe title='project 3' tagrte='_blank' src="https://nameless-journey-02981.herokuapp.com/"> Symptom Tracker </iframe>
+
         </div>
     )
 }
