dd9b142ce6afec62ab2a72725d2232d01e1545bc
Ariel Khawly <akhawly59@gmail.com>
2020-04-12 19:01:40 -0700

    fixed href, working on images for projects

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,8 +4,7 @@ import './assets/Footer.css'
 function Footer() {
     return (
         <footer className='footer'>
-            <p>Made by: Ariel Khawly</p>
-            <p>Contact information: <a href="mailto:akhawly1@gmail.com">
+            <p>Made by: Ariel Khawly | Contact information: <a href="mailto:akhawly1@gmail.com">
                 akhawly1@gmail.com</a>.</p>
         </footer>
     )
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,14 +1,16 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 function Projects() {
     return (
-        <div>
-            <iframe title='project 1' target='_blank' src="https://arielkhawly.github.io/project-1/"> Last Minute Date Generator </iframe>
-            <a src='https://arielkhawly.github.io/project-1/' target='_blank' rel="noopener noreferrer">Date Generator</a>
-            <iframe title='project 2' taget='_blank' src=" https://gentle-bayou-98216.herokuapp.com/"> The Boxing Office </iframe>
-            <iframe title='project 3' tagrte='_blank' src="https://nameless-journey-02981.herokuapp.com/"> Symptom Tracker </iframe>
+        <article>
+            {/* <img src='../components/assets/date'> </img> */}
+            <a title='project 1' target='_blank' rel="noopener noreferrer" href="https://arielkhawly.github.io/project-1/"> Last Minute Date Generator </a>
+            <img src=''></img>
+            <a title='project 2' taget='_blank' rel="noopener noreferrer" href=" https://gentle-bayou-98216.herokuapp.com/"> The Boxing Office </a>
+            <img src=''></img>
+            <a title='project 3' tagrte='_blank' rel="noopener noreferrer" href="https://nameless-journey-02981.herokuapp.com/"> Symptom Tracker </a>
 
-        </div>
+        </article>
     )
 }
 
