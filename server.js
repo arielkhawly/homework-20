6f19d4e9493ff715b0b52671782511fe1abb7a31
Ariel Khawly <akhawly59@gmail.com>
2020-04-12 19:55:04 -0700

    added backend

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const routes = require("./routes");
+const mongoose = require("mongoose");
+const accounts = require("./routes/api/accounts");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+app.use("/api", accounts)
+
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
